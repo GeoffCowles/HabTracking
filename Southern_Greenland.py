2434574dfde991d92dad8e980d799bf62d5c96bb
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-02-21 20:21:23 -0500

    Add files via upload

---
+++ Southern_Greenland.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 09:52:13 2022
+
+@author: lcabral4
+"""
+
+n = 10000#number of particles
+lonmin = -53.314722
+lonmax = -49.548333
+latmin = 61.821667
+latmax = 58.723889
+days = 600 #number of days to run code
+outputminutes=30 #output file minutes
+dtminutes = 1 #timestep minutes
+
+year = 2015 #year starting point
+month = 1 #minth starting point
+day = 1 #day starting point
