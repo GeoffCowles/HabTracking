a0ade9844d89d2a1633ffb32fb0386d59dbedbd6
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-02-22 19:04:12 -0500

    Add files via upload

---
+++ Bay_Of_Fundy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 19:02:25 2022
+
+@author: lcabral4
+"""
+
+n = 600#number of particles
+lonmin = -67.3075
+lonmax = -66.60889
+latmin = 44.353611
+latmax = 43.766389
+days = 600 #number of days to run code
+outputminutes=30 #output file minutes
+dtminutes = 1 #timestep minutes
+
+year = 2016 #year starting point
+month = 10 #minth starting point
+day = 1 #day starting point
