0b250f6d1d0fb582228ac5780b5072eff78fb94a
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-02-24 19:32:44 -0500

    Add files via upload

---
+++ Firth_Of_Lorn_5_1_15.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 24 19:30:52 2022
+
+@author: lcabral4
+"""
+
+n = 1000#number of particles
+lonmin = -8.898056
+lonmax = -7.258056
+latmin = 56.501944
+latmax = 55.482222
+days = 500 #number of days to run code
+outputminutes=30 #output file minutes
+dtminutes = 1 #timestep minutes
+
+year = 2015 #year starting point
+month = 5
+day = 1 #day starting point
