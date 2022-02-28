3b440b5cfab81af1cbd4be612a6a5582a55fba63
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-02-28 18:50:58 -0500

    Add files via upload

---
+++ Firth_Of_Lorne_3_1_2015.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 24 19:42:56 2022
+
+@author: lcabral4
+"""
+
+n = 600#number of particles
+lonmin = -5.881389
+lonmax = -5.845278
+latmin = 56.290833
+latmax = 56.213611
+days = 600 #number of days to run code
+outputminutes=30 #output file minutes
+dtminutes = 1 #timestep minutes
+
+year = 2015 #year starting point
+month = 3
+day = 1 #day starting point
