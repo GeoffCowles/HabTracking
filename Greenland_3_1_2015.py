741691bf37dc738cbd3740358c80924e138f36e4
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-03-02 14:14:27 -0500

    Add files via upload

---
+++ Greenland_3_1_2015.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 14:13:08 2022
+
+@author: lcabral4
+"""
+
+Np = 600#number of particles
+#line_start = [-60.,56.]
+#line_end = [-55.5,57.4]
+
+#cross-gom
+line_start = [-42.661111, 60.279444]
+line_end = [-43.206667, 59.823056]
+days = 600#number of days to run code
+outputminutes=30 #output file minutes
+dtminutes = 1 #timestep minutes
+
+year = 2015 #year starting point
+month = 3
+day = 1 #day starting point
