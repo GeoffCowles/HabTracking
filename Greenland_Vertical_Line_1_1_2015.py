62db182c4e79a77938645d49cab4d5dd1c23cec5
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-03-03 16:41:03 -0500

    Add files via upload

---
+++ Greenland_Vertical_Line_1_1_2015.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  3 16:38:56 2022
+
+@author: lcabral4
+"""
+
+Np = 600#number of particles
+#line_start = [-60.,56.]
+#line_end = [-55.5,57.4]
+
+#cross-gom
+line_start = [-43.213056, 59.600278]
+line_end = [-43.761389, 50.241667]
+days = 600#number of days to run code
+outputminutes=30 #output file minutes
+dtminutes = 30 #timestep minutes
+
+year = 2015 #year starting point
+month = 1
+day = 1 #day starting point
