2bef539112af0cc3a38ceca45273deb406affb42
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-03-03 12:21:40 -0500

    Add files via upload

---
+++ North_Line_1_1_2015.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  3 12:19:57 2022
+
+@author: lcabral4
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 11:26:24 2022
+
+@author: lcabral4
+"""
+
+Np = 600#number of particles
+#line_start = [-60.,56.]
+#line_end = [-55.5,57.4]
+
+#cross-gom
+line_start = [-34.551389, 65.432222]
+line_end = [-6.016667, 59.026944]
+days = 600#number of days to run code
+outputminutes=30 #output file minutes
+dtminutes = 1 #timestep minutes
+
+year = 2015 #year starting point
+month = 1
+day = 1 #day starting point
