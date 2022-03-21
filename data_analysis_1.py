2efa203f85da19106f8cd8b6546edf7a8239bca1
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-03-21 16:12:21 -0400

    Add files via upload

---
+++ data_analysis_1.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 16:02:56 2022
+
+@author: lcabral4
+"""
+
+from parcels import FieldSet, ParticleSet, JITParticle, AdvectionRK4, ErrorCode, Variable
+from datetime import timedelta, datetime
+import numpy as np
+import random
+import netCDF4
+import sys
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs 
+#from mpl_toolkits.basemap import Basemap
+from cartopy import config
+import cartopy.crs as ccrs
+import cartopy.mpl.ticker as cticker 
+from parcels import plotting
+import matplotlib.pylab as pl
+import cartopy.feature as cfeature
+#from x import *
+from matplotlib.colors import ListedColormap
+#from Southern_Greenland import *
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import os
+
+from netCDF4 import Dataset
+
+ncfile = '/home/lcabral4/WHOI/WHOI/float_output/box/box.nc'
+
+data = Dataset(ncfile)
+
+y = data.variables['lat'][:]
+
+x = data.variables['lon'][:]
+time = data.variables['time'][:]
+temp = data.variables['temp'][:]
+
+fig = plt.figure(figsize=(13,10))
+
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.set_extent([-74,-1, 70, 20], ccrs.PlateCarree())
+ax.coastlines()
+ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.COASTLINE)
+
+lon_formatter = cticker.LongitudeFormatter()
+ax.xaxis.set_major_formatter(lon_formatter)
+ax.set_xticks([-75, -70, -65, -60, -55, -50, -45, -40, -35, -30, -25, -20, -15, -10,-5, 0], crs=ccrs.PlateCarree())
+ax.set_yticks([30, 35, 40, 45, 50, 55, 60, 65, 70, 75], crs=ccrs.PlateCarree())
+
+lat_formatter = cticker.LatitudeFormatter()
+
+current_directory = os.getcwd()
+#final = open (current_directory + "/_rundata/box.log", "a")
+#final.write("x(lon)  y(lat)  temp(C)  \n")
+x1 = []
+x2 = []
+lat = []
+s = []
+t = []
+for p in range(x.shape[0]):
+    cb = plt.scatter(x[p,:], y[p,:], c=temp[p,:], s=0.0001, marker = "o")
+#    x1.append(x[p,0])
+#    x2.append(x[p,-1])
+    t.append(time[p,:])
+    
+    
+    plt.scatter(x[:,0], y[:,0]) 
+    
+np.set_printoptions(threshold=sys.maxsize)
+#print(x1, 'lon start')
+#print(x2, 'lon end')
+print(t, 'time')
+    
+
+
+from shapely.geometry import LineString, MultiLineString
+from shapely.geometry import Polygon, Point
+
+fig = plt.figure(figsize=(13,10))
+
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.set_extent([-74,-1, 70, 20], ccrs.PlateCarree())
+ax.coastlines()
+ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.COASTLINE)
+
+lon_formatter = cticker.LongitudeFormatter()
+ax.xaxis.set_major_formatter(lon_formatter)
+ax.set_xticks([-75, -70, -65, -60, -55, -50, -45, -40, -35, -30, -25, -20, -15, -10,-5, 0], crs=ccrs.PlateCarree())
+ax.set_yticks([30, 35, 40, 45, 50, 55, 60, 65, 70, 75], crs=ccrs.PlateCarree())
+
+lat_formatter = cticker.LatitudeFormatter()
+
+current_directory = os.getcwd()
+#final = open (current_directory + "/_rundata/box.log", "a")
+#final.write("x(lon)  y(lat)  temp(C)  \n")
+
+
+for p in range(x.shape[0]):
+    if np.any(x[p,:] >=-70) and np.any(x[p,:] <=-65) and np.any(y[p,:] <=45) and np.any(y[p,:] >=40):
+
+        
+        plt.scatter(x[p,:], y[p,:], c=temp[p,:], s=0.0001, marker = "o")
+        plt.scatter(x[:,0], y[:,0]) 
+
+
+#        final.write(str(x[p,:]) + " " + str(y[p,:]) + " " + str(temp[p,:]) + "\n")
+
+#final.close()
+c = plt.colorbar(shrink = .7855)
+plt.clim(-2.5, 27.5) 
+
+#print(temp, 'temp')
+coord = [[-70,40], [-70,45],[-65,45], [-65,40]]
+#poly = Polygon(((-70, 40), (-70, 45), (-65, 45), (-65, 40)))
+#print(poly)
+#poly = MultiPoint(coords).convex_hull
+coord.append(coord[0])
+xs, ys = zip(*coord) #create lists of x and y values 
+plt.plot(xs,ys) 
+
+
+# plot
+
+
+
+plt.title('Greenland')
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.savefig("Greenland_1_1_2015_1.png")    
+plt.show()
+
+from shapely.geometry import LineString, MultiLineString
+from shapely.geometry import Polygon, Point
+
+fig = plt.figure(figsize=(13,10))
+
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.set_extent([-74,-1, 70, 20], ccrs.PlateCarree())
+ax.coastlines()
+ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.COASTLINE)
+
+lon_formatter = cticker.LongitudeFormatter()
+ax.xaxis.set_major_formatter(lon_formatter)
+ax.set_xticks([-75, -70, -65, -60, -55, -50, -45, -40, -35, -30, -25, -20, -15, -10,-5, 0], crs=ccrs.PlateCarree())
+ax.set_yticks([30, 35, 40, 45, 50, 55, 60, 65, 70, 75], crs=ccrs.PlateCarree())
+
+lat_formatter = cticker.LatitudeFormatter()
+
+current_directory = os.getcwd()
+#final = open (current_directory + "/_rundata/box.log", "a")
+#final.write("x(lon)  y(lat)  temp(C)  \n")
+
+longitude = []
+for p in range(x.shape[0]):
+    if np.any(x[p,:] >=-70) and np.any(x[p,:] <=-65) and np.any(y[p,:] <=45) and np.any(y[p,:] >=40):
+
+        
+        plt.scatter(x[p,:], y[p,:], c=temp[p,:], s=0.0001, marker = "o")
+        plt.scatter(x[:,0], y[:,0]) 
+        print(x[p,0], 'lon start', y[p,0], 'lat start', x[p,-1], 'lon end', y[p,-1], 'lat end')
+        print(time[p,0], 'time start', time[p,-1], 'time end')
+        print(p)
+        print('-----------------------------------------------------------------------------')
+
+
+#        final.write(str(x[p,:]) + " " + str(y[p,:]) + " " + str(temp[p,:]) + "\n")
+
+#final.close()
+c = plt.colorbar(shrink = .7855)
+plt.clim(-2.5, 27.5) 
+
+#print(temp, 'temp')
+coord = [[-70,40], [-70,45],[-65,45], [-65,40]]
+#poly = Polygon(((-70, 40), (-70, 45), (-65, 45), (-65, 40)))
+#print(poly)
+#poly = MultiPoint(coords).convex_hull
+coord.append(coord[0])
+xs, ys = zip(*coord) #create lists of x and y values 
+plt.plot(xs,ys) 
+
+
+# plot
+
+
+
+plt.title('Greenland')
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.savefig("Greenland_1_1_2015_temp.png")    
+plt.show()
