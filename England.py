aeeb5cc227d6e16b88f15a43624f27677008807d
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-03-28 09:23:20 -0400

    Add files via upload

--- England.py
+++ England.py
@@ -6,15 +6,15 @@ Created on Tue Mar 15 10:35:55 2022
 @author: lcabral4
 """
 
-n = 10#number of particles
-lonmin = -18.743889
-lonmax = -0.213889
-latmin = 63.245883
-latmax = 58.61
-days = 10 #number of days to run code
+n = 600#number of particles
+lonmin = -16.92
+lonmax = -7.215556
+latmin = 63.010833
+latmax = 58.935833
+days = 850 #number of days to run code
 outputminutes=30 #output file minutes
-dtminutes = 1 #timestep minutes
+dtminutes = 60 #timestep minutes
 
-year = 2015 #year starting point
-month = 1 #minth starting point
+year = 2014 #year starting point
+month = 7 #minth starting point
 day = 1 #day starting point
