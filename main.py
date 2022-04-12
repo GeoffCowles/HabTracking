edfaec9dabda4be40e5cc6e1967051e0704e8c1d
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-04-12 13:21:08 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 21 14:17:37 2022
+Created on Tue Apr 12 13:17:39 2022
 
 @author: lcabral4
 """
-
 # Define the new Kernel that mimics RW
 def VerticalMovement(particle, fieldset, time):
-    
+
     # set particle salinity at new depth for output
     particle.salt =  fieldset.salt[time, particle.depth, particle.lat, particle.lon]
     particle.temp = fieldset.temp[time, particle.depth, particle.lat, particle.lon]
@@ -17,14 +16,14 @@ def VerticalMovement(particle, fieldset, time):
     # can code vertical diffusivity here with random walk
     # ocean parcels has some random walk functions as well
     # have particle.dt for time step, particle.depth for depth
-    
+
     # for now, lock to depth (surface)
 #    particle.depth = 0
 
 def OutOfBounds ( particle , fieldset , time ):
 
     particle.delete ()
-    
+
 from parcels import FieldSet, ParticleSet, JITParticle, AdvectionRK4, ErrorCode, Variable
 from datetime import timedelta, datetime
 import numpy as np
@@ -32,30 +31,31 @@ import random
 import netCDF4
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs 
+import cartopy.crs as ccrs
 #from mpl_toolkits.basemap import Basemap
 from cartopy import config
 import cartopy.crs as ccrs
-import cartopy.mpl.ticker as cticker 
+import cartopy.mpl.ticker as cticker
+
 from parcels import plotting
 import matplotlib.pylab as pl
 import cartopy.feature as cfeature
 #from x import *
-from Scotia import *
+from England import *
 
 
 # Load the HYCOM Analysis data in the Gulf of Maine
 # The paths/datafiles should be read in from a control file
-filenames = {'U': "./hycom_data/HAB2*",
-             'V': "./hycom_data/HAB2*",
-             'salt': "./hycom_data/HAB2*",
-             'temp': "./hycom_data/HAB2*"}
+filenames = {'U': "./hycom_data/HAB4*",
+             'V': "./hycom_data/HAB4*",
+             'salt': "./hycom_data/HAB4*",
+             'temp': "./hycom_data/HAB4*"}
 variables = {'U': 'water_u',
              'V': 'water_v',
              'salt': 'salinity',
              'temp': 'water_temp'}
 dimensions = {'lat': 'lat', 'lon': 'lon', 'time': 'time'}
-fieldset = FieldSet.from_netcdf(filenames, variables, dimensions,allow_time_extrapolation = True)
+fieldset = FieldSet.from_netcdf(filenames, variables, dimensions,allow_time_extrapolation = False)
 fieldset.mindepth = fieldset.U.depth[0]  # uppermost layer in the hydrodynamic data
 
 
@@ -73,7 +73,7 @@ print(fieldset.U.lon[0],fieldset.U.lon[-1],fieldset.U.lat[0],fieldset.U.lat[-1])
 #    cycle_age = Variable('cycle_age', dtype=np.float32, initial=0.)
 #    drift_age = Variable('drift_age', dtype=np.float32, initial=0.)
 #    S = Variable('S', dtype=np.float32, initial=np.nan)  # store salinity
-    
+
 # Define a new Particle type including extra Variables
 class HabParticle(JITParticle):
     # Phase of cycle: init_descend=0, drift=1, profile_descend=2, profile_ascend=3, transmit=4
@@ -88,10 +88,10 @@ class HabParticle(JITParticle):
 x = []
 y = []
 
-lonmin 
-lonmax 
-latmin 
-latmax 
+lonmin
+lonmax
+latmin
+latmax
 n
 days
 year
@@ -111,82 +111,21 @@ pset = ParticleSet(fieldset=fieldset, pclass=HabParticle, lon=x, lat=y, time=tim
 
 
 fig = plt.figure(figsize=(13,10))
+kernels = VerticalMovement + pset.Kernel(AdvectionRK4)
+output_file = pset.ParticleFile(name="float_output/box1", outputdt=timedelta(minutes=outputminutes))
+pset.execute(kernels, runtime=timedelta(days=days), dt=timedelta(minutes=dtminutes), output_file=output_file, recovery={ErrorCode.ErrorOutOfBounds: OutOfBounds})
 
-
-#for i in range(5):
-#    pset = ParticleSet.from_list(fieldset=fieldset,
-#                                pclass=HabParticle,
-#                                lon=[-62 - random.randint(0,5), -57 - random.randint(0,5)],
-#                                lat =[43 + random.randint(0,5), 38 + random.randint(0,5)],
-#                                depth =[0 + random.randint(0,15), 0+random.randint(0,15)],
-#                                time=[datetime(2015,9,1)])
-
-#print(days)
-kernels = VerticalMovement + pset.Kernel(AdvectionRK4) 
-output_file = pset.ParticleFile(name="float_output/Scotia", outputdt=timedelta(minutes=outputminutes))
-pset.execute(kernels, runtime=timedelta(days=days), dt=-timedelta(minutes=dtminutes), output_file=output_file, recovery={ErrorCode.ErrorOutOfBounds: OutOfBounds})
-    
 #pset.execute(kernels, runtime=timedelta(days=600.), dt=timedelta(minutes=-30), output_file=output_file, recovery={ErrorCode.ErrorOutOfBounds: OutOfBounds})
 
 output_file.export()  # export the trajectory data to a netcdf file
 
-nc = netCDF4.Dataset("float_output/Scotia.nc")
+nc = netCDF4.Dataset("float_output/box1.nc")
 
 x = nc.variables["lon"][:].squeeze()
 time = nc.variables["time"][:].squeeze()
+
 y = nc.variables["lat"][:].squeeze()
 z = nc.variables["z"][:].squeeze()
 salt = nc.variables["salt"][:].squeeze()
 temp = nc.variables["temp"][:].squeeze()
 nc.close()
-fig = plt.figure(figsize=(13,10))
-for p in range(x.shape[0]):
-    cb = plt.scatter(x[p,:], y[p,:], c=temp[p,:], s=0.01, marker = "o")
-    plt.scatter(x[:,0], y[:,0], s= 20, marker = 's') 
-plt.title('Scotia temperature ')
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
-plt.savefig("Scotia.png")
-    
-#plt.show()
-
-
-fig = plt.figure(figsize=(13,10))
-#    for p in range(x.shape[0]):
-#        cb = plt.scatter(x[p], y[p], c=temp[p], s=20, marker = "o")
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.set_extent([-74,-1, 70, 20], ccrs.PlateCarree())
-ax.coastlines()
-ax.add_feature(cfeature.LAND)
-ax.add_feature(cfeature.COASTLINE)
-#x = np.linspace(-65, -75)
-#y = np.linspace(35, 45)
-#plt.title('Temperature Variable')
-#ax.set_xlabel("Longitude")
-#ax.set_ylabel("Latitude")
-#ax.set_xticks(np.arange(-80,-65,100), crs=ccrs.PlateCarree())
-lon_formatter = cticker.LongitudeFormatter()
-ax.xaxis.set_major_formatter(lon_formatter)
-ax.set_xticks([-75, -70, -65, -60, -55, -50, -45, -40, -35, -30, -25, -20, -15, -10,-5, 0], crs=ccrs.PlateCarree())
-ax.set_yticks([30, 35, 40, 45, 50, 55, 60, 65, 70, 75], crs=ccrs.PlateCarree())
-#ax.set_yticks(np.arange(40,50,20), crs=ccrs.PlateCarree())
-lat_formatter = cticker.LatitudeFormatter()
-#ax.yaxis.set_major_formatter(lat_formatter) 
-#lon, lat = np.meshgrid(x, y)
-for p in range(x.shape[0]):
-    cb = plt.scatter(x[p,:], y[p,:], c=temp[p,:], s=0.0001, marker = "o")
-    plt.scatter(x[:,0], y[:,0]) 
-#print(temp, 'temp')
-
-
-
-# plot
-
-
-
-
-plt.title('Scotia Temperature')
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
-plt.savefig("Scotia_Temp.png")    
-#plt.show()
