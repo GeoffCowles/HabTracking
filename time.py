9f13167e5a07077833941f7d7d274169996fc8f6
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-04-16 21:42:56 -0400

    Add files via upload

--- time.py
+++ time.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr  5 12:16:53 2022
+Created on Sat Apr 16 21:38:54 2022
 
 @author: lcabral4
 """
@@ -14,11 +14,11 @@ import netCDF4
 import sys
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs 
+import cartopy.crs as ccrs
 #from mpl_toolkits.basemap import Basemap
 from cartopy import config
 import cartopy.crs as ccrs
-import cartopy.mpl.ticker as cticker 
+import cartopy.mpl.ticker as cticker
 from parcels import plotting
 import matplotlib.pylab as pl
 import cartopy.feature as cfeature
@@ -31,14 +31,21 @@ import os
 
 from netCDF4 import Dataset
 
-ncfile = '/home/lcabral4/WHOI/WHOI/float_output/test.nc'
+ncfile = '/home/lcabral4/WHOI/HAB3/HabTracking/float_output/box1.nc'
+
 
 data = Dataset(ncfile)
 
 y = data.variables['lat'][:]
+
 x = data.variables['lon'][:]
 time = data.variables['time'][:]
 temp = data.variables['temp'][:]
+
+from shapely.geometry import LineString, MultiLineString
+from shapely.geometry import Polygon, Point
+
+'''d_analysis.py'''
 
 fig = plt.figure(figsize=(13,10))
 
@@ -50,12 +57,12 @@ ax.add_feature(cfeature.COASTLINE)
 
 lon_formatter = cticker.LongitudeFormatter()
 ax.xaxis.set_major_formatter(lon_formatter)
-ax.set_xticks([-75, -70, -65, -60, -55, -50, -45, -40, -35, -30, -25, -20, -15, -10,-5, 0], crs=ccrs.PlateCarree())
-ax.set_yticks([30, 35, 40, 45, 50, 55, 60, 65, 70, 75], crs=ccrs.PlateCarree())
+ax.set_xticks([-75, -70, -65, -60, -55, -50, -45, -40, -35, -30, -25, -20, -15, -10,-5, 0, 5, 10, 15, 20, 25], crs=ccrs.PlateCarree())
+ax.set_yticks([30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85], crs=ccrs.PlateCarree())
+
 
 lat_formatter = cticker.LatitudeFormatter()
 
-current_directory = os.getcwd()
 
 starting_longitude = []
 ending_longitude = []
@@ -64,48 +71,24 @@ ending_time = []
 starting_lat = []
 ending_lat = []
 particle_number = []
-
-
-#print(longitude)
-#print(starting_time)
-
-
+t1 = []
+t2 = []
 for p in range(x.shape[0]):
-#    if np.any(time[p,0] == 0):
-    print(time[p,0])
-
-#    plt.scatter(x[p,:], y[p,:], c=temp[p,:], s=0.0001, marker = "o")
-#    plt.scatter(x[:,0], y[:,0]) 
-#plt.colorbar(shrink = .7855)
-#plt.clim(-2.5, 27.5) 
-
-#print(temp, 'temp')
-#coord = [[-70,40], [-70,45],[-65,45], [-65,40]]
-#coord = [[-20.4,63.2], [-20.4,62.5], [-20.2,62.5], [-20.2,63.5]]
-#poly = Polygon(((-70, 40), (-70, 45), (-65, 45), (-65, 40)))
-#print(poly)
-#poly = MultiPoint(coords).convex_hull
-#coord.append(coord[0])
-#xs, ys = zip(*coord) #create lists of x and y values 
-##plt.plot(xs,ys) 
+#    if np.any(x[p,:] >=-20.4) and np.any(x[p,:] <=-20.2) and np.any(y[p,:] <=63.26) and np.any(y[p,:] >=62.5):
 
 
-# plot
+    plt.scatter(x[p,:], y[p,:], c=temp[p,:], s=0.1, marker = "o")
+    plt.scatter(x[:,0], y[:,0])
+    t1.append(time[0])
+    t2.append(time[-1])
+    break
+print(t1, t2)
+#c = plt.colorbar(shrink = .7855)
+#plt.clim(-2.5, 27.5)
 
-#print(particle_number)
 
-#plt.title('Greenland')
-#plt.xlabel("Longitude")
-#plt.ylabel("Latitude")
-#plt.savefig("canada_box.png")    
-#plt.show()
-'''
-#print(longitude)
-#print(starting_time)
+plt.title('Release of 800 particles on 7/1/2014')
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.savefig("All_7_1_2014.png")
 
-dat = np.array([particle_number, starting_longitude, ending_longitude, starting_lat, ending_lat, starting_time, ending_time])
-
-dat = dat.T
-
-np.savetxt('England1.txt', dat ,header = 'particle number               starting longitude         ending longitude          starting latitude          ending latitude            start time              end time',delimiter = ',')
-'''
