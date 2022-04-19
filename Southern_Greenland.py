9929c5dbf75b8a409e5bbf81c3c7718c5db0fd9f
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-04-19 18:07:28 -0400

    Add files via upload

---
+++ Southern_Greenland.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 11:38:46 2022
+
+@author: lcabral4
+"""
+
+Np = 100#number of particles
+#line_start = [-60.,56.]
+#line_end = [-55.5,57.4]
+
+#cross-gom
+line_start = [-50, 60]
+line_end = [-40, 60]
+days = 50#number of days to run code
+outputminutes=30 #output file minutes
+dtminutes = 30 #timestep minutes
+
+year = 2015 #year starting point
+month = 1
+day = 1 #day starting point
