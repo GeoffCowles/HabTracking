1e2b3a408965bc92ea717c8c0d0808e46e61ffed
lcabral4 <65676242+lcabral4@users.noreply.github.com>
2022-04-20 10:44:52 -0400

    Add files via upload

---
+++ bay_of_fundy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 09:52:13 2022
+
+@author: lcabral4
+"""
+
+n = 1000#number of particles
+lonmin = -66.319167
+lonmax = -66.056111
+latmin = 44.97556
+latmax = 44.663611
+days = 89 #number of days to run code
+outputminutes=30 #output file minutes
+dtminutes = 1 #timestep minutes
+
+year = 2016 #year starting point
+month = 9 #minth starting point
+day = 30 #day starting point
